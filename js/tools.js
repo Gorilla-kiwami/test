2fcf06f1d8e66a4533785f10f339a87dbda124f6
Gorilla-kiwami <jyotestpoo@gmail.com>
2025-06-28 23:09:58 +0900

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -1,1 +1,7 @@
+const textInput = document.getElementById('textInput');
+const charCount = document.getElementById('charCount');
 
+textInput.addEventListener('input', () => {
+  const text = textInput.value;
+  charCount.textContent = text.length;
+});
